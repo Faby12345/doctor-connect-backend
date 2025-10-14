835867d2f85f94430ac321325cf3f150469269fc
Turle Fabian <fabyturlw@gmail.com>
2025-10-14 15:04:53 +0300

    adding the endpoint for doctor profile

--- src/main/java/app/doctor_connect_backend/doctor/DoctorController.java
+++ src/main/java/app/doctor_connect_backend/doctor/DoctorController.java
@@ -1,23 +1,37 @@
 package app.doctor_connect_backend.doctor;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import app.doctor_connect_backend.user.UserService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/doctor")
 public class DoctorController {
     private final DoctorService doctorService;
-    public DoctorController(DoctorService doctorService) {
+    private final UserService userService;
+    public DoctorController(DoctorService doctorService, UserService userService) {
         this.doctorService = doctorService;
+        this.userService = userService;
     }
     @GetMapping("/all")
     public List<DoctorDTO> findAll(){
         return doctorService.findAll();
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorDTO> getDoctorById(@PathVariable String id){
+        try{
+            var doctor = doctorService.findByUser_id(UUID.fromString(id));
+            var user = userService.findById(doctor.getUserId());
+            DoctorDTO dto = new DoctorDTO(doctor.getUserId(), user.getFullName(), doctor.getSpeciality(), doctor.getCity(), doctor.getPriceMinCents(), doctor.getPriceMaxCents(), doctor.isVerified(),doctor.getRatingAvg() ,doctor.getRatingCount());
+            return ResponseEntity.ok(dto);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
 
+    }
 
 }
